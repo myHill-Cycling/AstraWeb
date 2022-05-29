28b7bf28bae5f7aa6984f69008a653402a52c4b0
Vespion <21206333+vespion@users.noreply.github.com>
2022-05-29 18:45:33 +0100

    fix: site now passes lighthouse audits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
-
-import {readFile} from "fs/promises";
-
+import { readFile } from "fs/promises";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-
-import { imagetools } from "vite-imagetools";
-// import { VitePWA } from "vite-plugin-pwa";
+import { imagetools } from "vite-imagetools"; // import { VitePWA } from "vite-plugin-pwa";
 
 import favicons from "@myhill-cycling/astro-favicon-intergration";
 const faviconOptions = JSON.parse(await readFile("./assets/images/favi/faviconDescriptor.json"));
-
 import dotenv from "dotenv";
-dotenv.config({path: "./.env.local"});
+import critters from "astro-critters";
+dotenv.config({
+  path: "./.env.local"
+});
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,20 +23,18 @@ export default defineConfig({
     format: "file"
   },
   experimental: {
-    integrations: true,
-  },  
-  integrations: [preact(), tailwind(), favicons(faviconOptions, process.env.RFG_API_KEY, "./.cache/favi"), sitemap(), robotsTxt()],
+    integrations: true
+  },
+  integrations: [preact(), tailwind(), favicons(faviconOptions, process.env.RFG_API_KEY, "./.cache/favi"), sitemap(), robotsTxt(), critters()],
   vite: {
-    plugins: [
-      imagetools(),
-      // VitePWA({
-      //   manifest: false, //RFG already makes a manifest
-      //   strategies: 'injectManifest',
-      //   registerType: 'autoUpdate',
-      //   srcDir: './src/worker',
-      //   filename: 'sw.ts',
-      //   includeAssets: ["robots.txt"]
-      // })
+    plugins: [imagetools() // VitePWA({
+    //   manifest: false, //RFG already makes a manifest
+    //   strategies: 'injectManifest',
+    //   registerType: 'autoUpdate',
+    //   srcDir: './src/worker',
+    //   filename: 'sw.ts',
+    //   includeAssets: ["robots.txt"]
+    // })
     ],
     ssr: {
       external: ["svgo"]
--- src/components/shell/sw.tsx
+++
@@ -1,7 +0,0 @@
-//This file will contain SW registration code when the Vite plugin is fixed
-
-const SW = () => {
-  return null;
-};
-
-export default SW;
