81ec4568530d7f14aee246cf81ea6ac7e5043acd
Vespion <21206333+vespion@users.noreply.github.com>
2022-06-05 02:45:27 +0100

    feat: stage 1 of switch to favicon intergration over custom built package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from "astro/config";
-import { readFile } from "fs/promises";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import { imagetools } from "vite-imagetools"; // import { VitePWA } from "vite-plugin-pwa";
-
-import favicons from "@myhill-cycling/astro-favicon-intergration";
-const faviconOptions = JSON.parse(await readFile("./assets/images/favi/faviconDescriptor.json"));
-import dotenv from "dotenv";
+import { imagetools } from "vite-imagetools";
 import critters from "astro-critters";
-dotenv.config({
-  path: "./.env.local"
-});
+import favicon from "astro-webmanifest";
 
 
 // https://astro.build/config
@@ -25,15 +18,50 @@ export default defineConfig({
   experimental: {
     integrations: true
   },
-  integrations: [preact(), tailwind(), favicons(faviconOptions, process.env.RFG_API_KEY, "./.cache/favi"), sitemap(), robotsTxt(), critters()],
+  integrations: [preact(), tailwind(), sitemap(), robotsTxt(), critters(), favicon({
+      icon: "./assets/images/favicon.png",
+      config: {
+        iconPurpose: ["maskable", "any"],
+        insertAppleTouchLinks: true
+      },
+      name: "myHill Cycling",
+      description: "Sportives and weekend training camps set in the peak district to get more people on their bikes!",
+      developerName: "Vespion Softworks",
+      //developerURL: "", // prevent retrieving from the nearest package.json
+      background: "#ffffff",
+      theme_color: "#f70702",
+      display: "browser",
+      start_url: "/",
+      scope: "/"
+  })],
   vite: {
-    plugins: [imagetools() // VitePWA({
-    //   manifest: false, //RFG already makes a manifest
-    //   strategies: 'injectManifest',
-    //   registerType: 'autoUpdate',
-    //   srcDir: './src/worker',
-    //   filename: 'sw.ts',
-    //   includeAssets: ["robots.txt"]
+    plugins: [imagetools() // favicons({
+    //   inject: true,
+    //   cache: true,
+    
+    //   path: "./assets/favi",
+    //   icons: {
+    //     android: {
+    //       mask: true,
+    //       source: "./assets/images/favicon.png"
+    //     },
+    //     windows: {
+    //       mask: true,
+    //       source: "./assets/images/favicon.png"
+    //     },
+    //     appleIcon: {
+    //       mask: true,
+    //       source: "./assets/images/favicon.png"
+    //     },
+    //     appleStartup: {
+    //       mask: true,
+    //       source: "./assets/images/favicon.png"
+    //     },
+    //     favicons: {
+    //       mask: true,
+    //       source: "./assets/images/favicon.png"
+    //     }
+    //   }
     // })
     ],
     ssr: {
