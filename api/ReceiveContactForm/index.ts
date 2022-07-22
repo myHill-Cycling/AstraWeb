9d8607c24c9c5111498fc6e7858a5e943de3a377
Vespion <21206333+vespion@users.noreply.github.com>
2022-07-22 02:16:15 +0100

    refactor(api): add additional log statments

--- api/ReceiveContactForm/index.ts
+++ api/ReceiveContactForm/index.ts
@@ -32,16 +32,30 @@ const MessageCheck = v.compile(MessageSchema);
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest) {
 
-	const parsed = new URLSearchParams(req.rawBody);
-	const dto: DTO = {
-		name: parsed.get("name"),
-		email: parsed.get("email"),
-		subject: parsed.get("subject"),
-		message: parsed.get("message")
-	};
+	context.log.info("Started processing form trigger");
+	let dto: DTO;
+	try {
+		const parsed = new URLSearchParams(req.rawBody);
+		dto = {
+			name: parsed.get("name"),
+			email: parsed.get("email"),
+			subject: parsed.get("subject"),
+			message: parsed.get("message")
+		};
+		context.log.info("Parsed data from request", dto);
+	}
+	catch(e) {
+		return {
+			status: 400,
+			body: {
+				status: "parsing-error"
+			}
+		};
+	}
 
 	const passedChecks = await MessageCheck(dto);
 	if(passedChecks !== true){
+		context.log.warn("Request data failed validation!", passedChecks);
 		return {
 			status: 400,
 			body: {
@@ -60,6 +74,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 		address: process.env.CONTACT_ADDRESS_EMAIL ?? ""
 	};
 
+	context.log.info("Address that the message will be sent to, ", contactUsAddress);
+
 	const systemAddress: Address = {
 		/* c8 ignore next */
 		name: process.env.SYSTEM_ADDRESS_NAME ?? "System NO-REPLY",
@@ -67,14 +83,18 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 		address: process.env.SYSTEM_ADDRESS_EMAIL ?? ""
 	};
 
+	context.log.info("Address that the message will be sent from, ", systemAddress);
+
+	const userAddress: Address = {
+		name: msgData.name,
+		address: msgData.email
+	};
+
 	const mailBuilder = new MailComposer({
 		from: systemAddress,
-		sender: systemAddress,
+		sender: userAddress,
 		to: contactUsAddress,
-		replyTo: {
-			name: msgData.name,
-			address: msgData.email
-		},
+		replyTo: userAddress,
 		subject: `${msgData.name} has a query! '${msgData.subject}'`,
 		text: msgData.message ?? ""
 	});
@@ -82,24 +102,31 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 	/* c8 ignore next */
 	const parsedSmtpPort = parseInt(process.env.SMTP_PORT ?? "");
 
-	const mailTransport = createTransport({
+	const transportConfig = {
 		/* c8 ignore next */
 		host: process.env.SMTP_HOST ?? "localhost",
 		/* c8 ignore next */
 		port: isNaN(parsedSmtpPort) ? 1025 : parsedSmtpPort,
 		secure: false, // upgrade later with STARTTLS
 		requireTLS: process.env.NODE_ENV === "production",
+		transactionLog: true,
+		debug: process.env.NODE_ENV === "development"
+	};
+
+	context.log.info("SMTP Connection configuration", transportConfig);
+
+	const mailTransport = createTransport({
+		...transportConfig,
 		auth: {
 			user: process.env.SMTP_USER,
 			pass: process.env.SMTP_PASS
 		},
-		logger: new SMTPLogger(context.log),
-		transactionLog: true,
-		debug: process.env.NODE_ENV === "development"
+		logger: new SMTPLogger(context.log)
 	});
 
 	let response: SentMessageInfo;
 	try {
+		context.log.info("Sending mail", mailBuilder.mail);
 		response = await mailTransport.sendMail(mailBuilder.mail);
 	}
 	catch (e) {
@@ -129,8 +156,10 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 		returnObj.body.response = response;
 	}
 
+	context.log.info("Closing SMTP connection");
 	mailTransport.close();
 
+	context.log.info("Response to submission", returnObj);
 	return returnObj;
 };
 
